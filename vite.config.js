bd901e8159d52a22be9004f45e07c6aaba82efec
zack-das <izackdas@gmail.com>
2025-09-10 14:16:48 +0300

    Allow render.com domain in vite preview

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview.allowedHosts: ['xdevscodehub-1sd.onrender.com']
+  preview: {
+    allowedHosts: ['xdevscodehub-1sd.onrender.com']
+  }
 })
